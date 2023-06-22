f46b8ee47ee6137cb15fbc1fb78905268b379291
Andrew Nowacki <andrew.p.nowacki@gmail.com>
2023-06-22 16:00:05 -0400

    pushing

--- web/components/seo.js
+++ web/components/seo.js
@@ -7,7 +7,8 @@ const SEO = ({ title, description, image, url, keywords }) => {
 
   const seoImage = image ? image : 'https://i.imgur.com/oYDGbYr.png';
   const seoTitle = title ? `${title} | Eight Branches` : `Eight Branches`
-
+  const seoUrl = url ? url : "https://www.eightbranches.ca/"
+  
   return (
     <Head>
       <title>{seoTitle}</title>
@@ -16,14 +17,14 @@ const SEO = ({ title, description, image, url, keywords }) => {
       <meta property="og:title" content={seoTitle} />
       <meta property="og:description" content={description} />
       <meta property="og:image" content={seoImage} />
-      <meta property="og:url" content={url} />
+      <meta property="og:url" content={seoUrl} />
       <meta property="og:site_name" content={siteName} />
       <meta name="twitter:title" content={seoTitle} />
       <meta name="twitter:description" content={description} />
       <meta name="twitter:image" content={seoImage} />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="theme-color" content="#316C5F" />
-      <link rel="canonical" href={url} />
+      <link rel="canonical" href={seoUrl} />
       <link rel="icon" href="/favicon-32x32.png" />
       <meta charSet="UTF-8" />
     </Head>
