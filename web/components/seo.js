96e0d0fab287b377a7585a3cd02176527092d799
Andrew Nowacki <andrew.p.nowacki@gmail.com>
2023-06-27 10:01:35 -0400

    adding favicons

--- web/components/seo.js
+++ web/components/seo.js
@@ -25,7 +25,10 @@ const SEO = ({ title, description, image, url, keywords }) => {
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="theme-color" content="#316C5F" />
       <link rel="canonical" href={seoUrl} />
-      <link rel="icon" href="/favicon-32x32.png" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="manifest" href="/site.webmanifest" />
       <meta charSet="UTF-8" />
     </Head>
   );
